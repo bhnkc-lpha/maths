7191fc3cf1368933f4bcb6912ee22c130a18ef7e
bhnkc-lpha <lpha@bhnkc.edu.hk>
2025-12-09 08:50:24 +0800

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import AngleQuiz from './apps/AngleQuiz';
+
+function App() {
+  return (
+    <BrowserRouter basename="/maths">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/angle-quiz" element={<AngleQuiz />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
